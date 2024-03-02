2dcbd626954ab0950583c3559d01338e87cd7d32
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-03-02 17:42:51 +0400

    commit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 const Button = () => {
   return (
-    <div className="w-[90%] bg-[#5E54A4] rounded-xl shadov-lg h-[88px] flex items-center justify-center md:h-[60px]">
+    <div className="w-[90%] bg-[#5E54A4] rounded-xl shadov-lg h-[88px] flex items-center justify-center md:h-[60px] btnn">
       <p className="text-white w-[70%] text-center text-base font-normal tracking-wide">
         <span className="font-bold leading-[26px]">Try it free 7 days </span>
         then $20/mo. there after
